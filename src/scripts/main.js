1d7fb6cd4770e4fcf31f3b95b7ee43eec1245f88
Nayara <nayaramaisa.stn@gmail.com>
2024-12-12 09:28:22 -0300

    commit final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 AOS.init();
+
+const dataDoEvento = new Date("Feb 16, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMS = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMS);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMS) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundoAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundoAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Esperar ate o proximo ano....'
+    }
+}, 1000);
